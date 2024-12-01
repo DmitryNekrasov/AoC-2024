41b695aaaf416e679cd9aa2f16d18f40c0659818
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-01 12:15:04 +0400

    Day01, part2: AC

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,8 +9,12 @@ fun List<String>.splitIntoTwoLists() =
 fun part1(list1: List<Int>, list2: List<Int>) =
     (list1.sorted() zip list2.sorted()).sumOf { (v1, v2) -> abs(v1 - v2) }
 
-fun part2(list1: List<Int>, list2: List<Int>): Long {
-    return -1
+fun part2(list1: List<Int>, list2: List<Int>): Int {
+    val counts = HashMap<Int, Int>()
+    for (v in list2) {
+        counts[v] = (counts[v] ?: 0) + 1
+    }
+    return list1.sumOf { it * (counts[it] ?: 0) }
 }
 
 fun main() {
@@ -32,10 +36,10 @@ fun main() {
 
         /**
          * Part 2
-         * Output: 2
+         * Output: 31
          */
         run {
-            val expected = 2
+            val expected = 31
             val actual = part2(list1, list2)
             assertEquals(expected, actual)
         }
