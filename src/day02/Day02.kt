dea9df2791fa6f29d539c7062892b37a96087057
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-02 12:10:41 +0400

    Day02, part2: WA

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -11,17 +11,35 @@ fun part1(input: List<List<Int>>): Int {
     fun List<Int>.isSafe(): Boolean {
         val compare = this[0] compareTo this[1]
         for (i in 1..lastIndex) {
-            if ((this[i - 1] compareTo this[i]) != compare || abs(this[i - 1] - this[i]) > 3) {
+            if (this[i - 1] compareTo this[i] != compare || abs(this[i - 1] - this[i]) > 3) {
                 return false
             }
         }
         return true
     }
+
     return input.count { it.isSafe() }
 }
 
 fun part2(input: List<List<Int>>): Int {
-    return input.size
+    fun List<Int>.isSafe(ignoreIndex: Int): Boolean {
+        val compare = when (ignoreIndex) {
+            0 -> this[1] compareTo this[2]
+            1 -> this[0] compareTo this[2]
+            else -> this[0] compareTo this[1]
+        }
+        var prev = if (ignoreIndex == 0) this[1] else this[0]
+        for (i in (if (ignoreIndex > 2) 1 else 2)..lastIndex) {
+            if (i == ignoreIndex) continue
+            if (prev compareTo this[i] != compare || abs(prev - this[i]) > 3) {
+                return false
+            }
+            prev = this[i]
+        }
+        return true
+    }
+
+    return input.count { list -> (0..list.lastIndex).any { list.isSafe(it) } }
 }
 
 fun main() {
@@ -35,7 +53,7 @@ fun main() {
         }
 
         run {
-            val expected = -1
+            val expected = 4
             val actual = part2(input)
             assertEquals(expected, actual)
         }
