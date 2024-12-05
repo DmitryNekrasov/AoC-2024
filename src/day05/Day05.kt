bd52ff3feef785c7ebed99989f2e78f29584b531
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-05 14:57:31 +0400

    Day05, part2: Preparation

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -16,55 +16,54 @@ fun List<String>.parse(): Pair<Set<Pair<Int, Int>>, List<List<Int>>> {
     return pageOrdering.parseRules() to updateString.parseSequences()
 }
 
-fun part1(rules: Set<Pair<Int, Int>>, sequences: List<List<Int>>): Int {
-    var result = 0
-    for (sequence in sequences) {
-        var isCorrect = true
-        for (i in 0..<sequence.lastIndex) {
-            val from = sequence[i]
-            for (j in (i + 1)..sequence.lastIndex) {
-                val to = sequence[j]
-                if (from to to !in rules) {
-                    isCorrect = false
-                }
+fun List<List<Int>>.partition(rules: Set<Pair<Int, Int>>) = this.partition { sequence ->
+    var isCorrect = true
+    for (i in 0..<sequence.lastIndex) {
+        val from = sequence[i]
+        for (j in (i + 1)..sequence.lastIndex) {
+            val to = sequence[j]
+            if (from to to !in rules) {
+                isCorrect = false
             }
         }
-        if (isCorrect) {
-            result += sequence[sequence.size / 2]
-        }
     }
-    return result
+    isCorrect
 }
 
-fun part2(rules: Set<Pair<Int, Int>>, sequences: List<List<Int>>): Int {
+fun part1(sequences: List<List<Int>>): Int {
+    return sequences.sumOf { it[it.size / 2] }
+}
+
+fun part2(rules: Set<Pair<Int, Int>>, incorrect: List<List<Int>>): Int {
+    println("rules = $rules")
+    println("incorrect = $incorrect")
+
     return rules.size
 }
 
 fun main() {
     run {
         val (rules, sequences) = readInput("Day05_test01").parse()
-        println("rules = $rules")
-        println("sequences = $sequences")
+        val (correct, incorrect) = sequences.partition(rules)
 
         run {
             val expected = 143
-            val actual = part1(rules, sequences)
+            val actual = part1(correct)
             assertEquals(expected, actual)
         }
 
         run {
-            val expected = -1
-            val actual = part2(rules, sequences)
+            val expected = 123
+            val actual = part2(rules, incorrect)
             assertEquals(expected, actual)
         }
     }
 
     run {
         val (rules, sequences) = readInput("Day05").parse()
-        println("rules = $rules")
-        println("sequences = $sequences")
+        val (correct, incorrect) = sequences.partition(rules)
 
-        part1(rules, sequences).println()
-        part2(rules, sequences).println()
+        part1(correct).println()
+        part2(rules, incorrect).println()
     }
 }
