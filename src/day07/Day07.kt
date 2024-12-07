0d84b0cffbc701664e198ee5f155c918552bba6f
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-07 16:08:22 +0400

    Day07, part02: AC

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -19,8 +19,47 @@ fun part1(input: List<String>): BigInteger {
     return input.map(String::parseLine).filter { (target, values) -> check(target, values) }.sumOf { it.first }
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(input: List<String>): BigInteger {
+    val cache = HashMap<Int, List<List<Char>>>()
+
+    fun generateOperators(n: Int, current: MutableList<Char> = mutableListOf()): List<List<Char>> {
+        if (n == 0) return listOf(current.toList())
+        val result = mutableListOf<List<Char>>()
+
+        current += '+'
+        result += generateOperators(n - 1, current)
+        current.removeAt(current.lastIndex)
+
+        current += '*'
+        result += generateOperators(n - 1, current)
+        current.removeAt(current.lastIndex)
+
+        current += '|'
+        result += generateOperators(n - 1, current)
+        current.removeAt(current.lastIndex)
+
+        return result
+    }
+
+    infix fun BigInteger.concat(other: BigInteger) = BigInteger("${this}${other}")
+
+    fun perform(values: List<BigInteger>, operators: List<Char>): BigInteger {
+        var result = values.first()
+        for ((operator, value) in operators zip values.slice(1..values.lastIndex)) {
+            when (operator) {
+                '+' -> result += value
+                '*' -> result *= value
+                '|' -> result = result concat value
+                else -> throw UnsupportedOperationException("Unsupported operation $operator")
+            }
+        }
+        return result
+    }
+
+    return input.map(String::parseLine).filter { (target, values) ->
+        cache.getOrPut(values.size - 1) { generateOperators(values.size - 1) }
+            .any { operators -> perform(values, operators) == target }
+    }.sumOf { it.first }
 }
 
 fun main() {
