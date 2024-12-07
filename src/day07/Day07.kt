00effe2f71682c1263c183960cb030a3fbd2ed65
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-07 15:20:05 +0400

    Day07, part01: AC

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -3,9 +3,20 @@ package day07
 import assertEquals
 import println
 import readInput
+import java.math.BigInteger
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun String.parseLine(): Pair<BigInteger, List<BigInteger>> {
+    val (target, values) = split(":")
+    return BigInteger(target) to values.trim().split(" ").map { BigInteger(it) }
+}
+
+fun check(target: BigInteger, values: List<BigInteger>, current: BigInteger = values.first(), index: Int = 1): Boolean {
+    if (index == values.size) return current == target
+    return check(target, values, current + values[index], index + 1) || check(target, values, current * values[index], index + 1)
+}
+
+fun part1(input: List<String>): BigInteger {
+    return input.map(String::parseLine).filter { (target, values) -> check(target, values) }.sumOf { it.first }
 }
 
 fun part2(input: List<String>): Int {
@@ -17,7 +28,7 @@ fun main() {
         val input = readInput("Day07_test01")
 
         run {
-            val expected = -1
+            val expected = BigInteger.valueOf(3749)
             val actual = part1(input)
             assertEquals(expected, actual)
         }
