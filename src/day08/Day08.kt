fdbe2850e70ce4fa49cd84f2e688fb040e6daf25
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-07 16:30:36 +0400

    Day08: Initial

---
+++ src/day08/Day08.kt
@@ -0,0 +1,36 @@
+package day08
+
+import assertEquals
+import println
+import readInput
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
+fun main() {
+    run {
+        val input = readInput("Day08_test01")
+
+        run {
+            val expected = -1
+            val actual = part1(input)
+            assertEquals(expected, actual)
+        }
+
+        run {
+            val expected = -1
+            val actual = part2(input)
+            assertEquals(expected, actual)
+        }
+    }
+
+    run {
+        val input = readInput("Day08")
+        part1(input).println()
+        part2(input).println()
+    }
+}
