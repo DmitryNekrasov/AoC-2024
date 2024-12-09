750eeed75544ce97f02a6665081e40cb77a18033
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-09 10:26:09 +0400

    Day09, part1: AC

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -4,8 +4,33 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: String): Int {
-    return input.length
+fun Int.repeat(n: Int) = List(n) { this }
+
+fun part1(input: String): Long {
+    val blocks = input.withIndex().fold(mutableListOf<Int>()) { acc, p ->
+        acc.also {
+            acc += (if ((p.index and 1) == 0) p.index / 2 else -1).repeat(p.value.code - '0'.code)
+        }
+    }
+
+    var p1 = 0
+    var p2 = blocks.lastIndex
+    while (p1 < p2) {
+        if (blocks[p1] != -1) {
+            p1++
+            continue
+        }
+        if (blocks[p2] == -1) {
+            p2--
+            continue
+        }
+        blocks[p1] = blocks[p2]
+        blocks[p2] = -1
+        p1++
+        p2--
+    }
+
+    return blocks.filter { it != -1 }.withIndex().sumOf { (index, value) -> index * value.toLong() }
 }
 
 fun part2(input: String): Int {
@@ -17,7 +42,7 @@ fun main() {
         val input = readInput("Day09_test01").first()
 
         run {
-            val expected = 1928
+            val expected = 1928L
             val actual = part1(input)
             assertEquals(expected, actual)
         }
