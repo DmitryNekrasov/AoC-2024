6294368070ce932d86f4947f5ac6ff12ec8a7f51
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-10 12:17:42 +0400

    Day10, part1: AC

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -4,8 +4,43 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun List<String>.getTrailheadScore(startI: Int, startJ: Int): Int {
+    val n = size
+    val m = first().length
+    val visited = Array(n) { BooleanArray(m) }
+    val finals = HashSet<Pair<Int, Int>>()
+
+    fun dfs(i: Int, j: Int) {
+        if (this[i][j] == '9') {
+            finals += i to j
+        } else {
+            visited[i][j] = true
+            for ((di, dj) in listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)) {
+                val nextI = i + di
+                val nextJ = j + dj
+                if (nextI in 0..<n && nextJ in 0..<m && this[nextI][nextJ] - this[i][j] == 1 && !visited[nextI][nextJ]) {
+                    dfs(nextI, nextJ)
+                }
+            }
+            visited[i][j] = false
+        }
+    }
+
+    dfs(startI, startJ)
+
+    return finals.size
+}
+
+fun part1(grid: List<String>): Int {
+    var result = 0
+    for (i in grid.indices) {
+        for (j in grid.first().indices) {
+            if (grid[i][j] == '0') {
+                result += grid.getTrailheadScore(i, j)
+            }
+        }
+    }
+    return result
 }
 
 fun part2(input: List<String>): Int {
