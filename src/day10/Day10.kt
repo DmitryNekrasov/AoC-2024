c880abc41ae4cda312f60ef50c009045ba5c8755
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-10 11:57:26 +0400

    Day10, part1: Actualize expected

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -17,7 +17,7 @@ fun main() {
         val input = readInput("Day10_test01")
 
         run {
-            val expected = -1
+            val expected = 36
             val actual = part1(input)
             assertEquals(expected, actual)
         }
