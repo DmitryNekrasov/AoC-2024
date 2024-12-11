a026e639de27c25fef1ef1bd5482a0ea9f706bbc
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-11 11:40:26 +0400

    Day11, part1: fix input, add expected

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -4,20 +4,20 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<String>): Int {
+fun part1(input: List<Int>): Int {
     return input.size
 }
 
-fun part2(input: List<String>): Int {
+fun part2(input: List<Int>): Int {
     return input.size
 }
 
 fun main() {
     run {
-        val input = readInput("Day11_test01")
+        val input = readInput("Day11_test01").first().split(" ").map(String::toInt)
 
         run {
-            val expected = -1
+            val expected = 55312
             val actual = part1(input)
             assertEquals(expected, actual)
         }
@@ -30,7 +30,7 @@ fun main() {
     }
 
     run {
-        val input = readInput("Day11")
+        val input = readInput("Day11").first().split(" ").map(String::toInt)
         part1(input).println()
         part2(input).println()
     }
