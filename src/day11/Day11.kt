3209f5120de8fe2a0d0c5bea8e59a90252fb96e9
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-11 12:15:54 +0400

    Day11, part1: Add Long.split()

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -4,17 +4,52 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<Int>): Int {
-    return input.size
+val Long.numberOfDigits: Int
+    get() {
+        if (this == 0L) return 1
+        var n = this
+        var count = 0
+        while (n > 0L) {
+            n /= 10
+            count++
+        }
+        return count
+    }
+
+val Long.hasAnEvenNumberOfDigits: Boolean
+    get() = (numberOfDigits and 1) == 0
+
+fun Long.split(): List<Long> {
+    var left = this
+    var right = 0L
+    var d = 1
+    repeat(numberOfDigits / 2) {
+        right += d * (left % 10)
+        d *= 10
+        left /= 10
+    }
+    return listOf(left, right)
 }
 
-fun part2(input: List<Int>): Int {
+fun part1(nums: List<Long>): Int {
+    for (num in nums) {
+        println("$num: ${num.hasAnEvenNumberOfDigits}")
+        if (num.hasAnEvenNumberOfDigits) {
+            val (left, right) = num.split()
+            println("left = $left, right = $right")
+        }
+    }
+
+    return nums.size
+}
+
+fun part2(input: List<Long>): Int {
     return input.size
 }
 
 fun main() {
     run {
-        val input = readInput("Day11_test01").first().split(" ").map(String::toInt)
+        val input = readInput("Day11_test01").first().split(" ").map(String::toLong)
 
         run {
             val expected = 55312
@@ -30,7 +65,7 @@ fun main() {
     }
 
     run {
-        val input = readInput("Day11").first().split(" ").map(String::toInt)
+        val input = readInput("Day11").first().split(" ").map(String::toLong)
         part1(input).println()
         part2(input).println()
     }
