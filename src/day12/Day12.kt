4fd05caff59cd71e76cbd915d261aff4a03d75f9
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-12 11:35:31 +0400

    Day12, part1: AC

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -5,7 +5,37 @@ import println
 import readInput
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val grid = input.map { it.toCharArray() }
+    val n = grid.size
+    val m = grid.first().size
+
+    val visited = Array(n) { BooleanArray(m) }
+
+    fun dfs(i: Int, j: Int, c: Char): Pair<Int, Int> {  // area -> perimeter
+        if (i !in 0..<n || j !in 0..<m || grid[i][j] != c) return 0 to 1
+        if (visited[i][j]) return 0 to 0
+        visited[i][j] = true
+        var totalArea = 1
+        var totalPerimeter = 0
+        for ((di, dj) in listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)) {
+            val (area, perimeter) = dfs(i + di, j + dj, c)
+            totalArea += area
+            totalPerimeter += perimeter
+        }
+        return totalArea to totalPerimeter
+    }
+
+    var result = 0
+    for (i in 0..<n) {
+        for (j in 0..<m) {
+            if (!visited[i][j]) {
+                val (area, perimeter) = dfs(i, j, grid[i][j])
+                result += area * perimeter
+            }
+        }
+    }
+
+    return result
 }
 
 fun part2(input: List<String>): Int {
