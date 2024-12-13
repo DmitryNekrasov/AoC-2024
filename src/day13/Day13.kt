970e26cc4f89c92a084d9251f39530f92e10baca
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-13 12:13:14 +0400

    Day13: Add parsing

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -3,18 +3,41 @@ package day13
 import assertEquals
 import println
 import readInput
+import kotlin.collections.plusAssign
 
-fun part1(input: List<String>): Int {
-    return input.size
+data class Equation(val aX: Int, val aY: Int, val bX: Int, val bY: Int, val targetX: Int, val targetY: Int)
+
+fun List<String>.parseEquation(): Equation {
+    val list = this.flatMap { line ->
+        line.split(":").last().trim().split(", ").map { it.substring(2) }.map(String::toInt)
+    }
+    return Equation(list[0], list[1], list[2], list[3], list[4], list[5])
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun List<String>.parse(): List<Equation> {
+    return this.fold(mutableListOf<MutableList<String>>().apply { add(mutableListOf()) }) { acc, value ->
+        acc.apply {
+            when (value) {
+                "" -> this += mutableListOf<String>()
+                else -> last() += value
+            }
+        }
+    }.map(List<String>::parseEquation)
+}
+
+fun part1(equations: List<Equation>): Int {
+    println(equations.joinToString("\n"))
+
+    return equations.size
+}
+
+fun part2(equations: List<Equation>): Int {
+    return equations.size
 }
 
 fun main() {
     run {
-        val input = readInput("Day13_test01")
+        val input = readInput("Day13_test01").parse()
 
         run {
             val expected = -1
@@ -30,7 +53,7 @@ fun main() {
     }
 
     run {
-        val input = readInput("Day13")
+        val input = readInput("Day13").parse()
         part1(input).println()
         part2(input).println()
     }
