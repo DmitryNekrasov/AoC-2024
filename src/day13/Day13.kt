eed65004ba36424647613bac9594259f254060da
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-13 12:17:53 +0400

    Day13, part1: Refactoring

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -9,7 +9,7 @@ data class Equation(val aX: Int, val aY: Int, val bX: Int, val bY: Int, val targ
 
 fun List<String>.parseEquation(): Equation {
     val list = this.flatMap { line ->
-        line.split(":").last().trim().split(", ").map { it.substring(2) }.map(String::toInt)
+        line.split(": ").last().split(", ").map { it.substring(2) }.map(String::toInt)
     }
     return Equation(list[0], list[1], list[2], list[3], list[4], list[5])
 }
