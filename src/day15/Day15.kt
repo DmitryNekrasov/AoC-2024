5afa0947deb9ad841702c68bfb9c3686e8d209c8
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-15 11:46:33 +0400

    Day15, part1: Add empty shifts methods

--- src/day15/Day15.kt
+++ src/day15/Day15.kt
@@ -7,7 +7,51 @@ import readInput
 fun List<String>.parse() =
     partition { it.startsWith("#") }.let { (first, second) -> first.map(String::toCharArray) to second.joinToString("") }
 
+fun List<CharArray>.shiftLeft(startI: Int, startJ: Int): Pair<Int, Int> {
+    // TODO
+    return -1 to -1
+}
+
+fun List<CharArray>.shiftRight(startI: Int, startJ: Int): Pair<Int, Int> {
+    // TODO
+    return -1 to -1
+}
+
+fun List<CharArray>.shiftTop(startI: Int, startJ: Int): Pair<Int, Int> {
+    // TODO
+    return -1 to -1
+}
+
+fun List<CharArray>.shiftBottom(startI: Int, startJ: Int): Pair<Int, Int> {
+    // TODO
+    return -1 to -1
+}
+
+val List<CharArray>.start: Pair<Int, Int>
+    get() {
+        for (i in indices) {
+            for (j in first().indices) {
+                if (this[i][j] == '@') {
+                    return i to j
+                }
+            }
+        }
+        throw RuntimeException("Should not reach here")
+    }
+
 fun part1(grid: List<CharArray>, commands: String): Int {
+    var (i, j) = grid.start
+    for (command in commands) {
+        val (nextI, nextJ) = when (command) {
+            '<' -> grid.shiftLeft(i, j)
+            '>' -> grid.shiftRight(i, j)
+            '^' -> grid.shiftTop(i, j)
+            'v' -> grid.shiftBottom(i, j)
+            else -> throw RuntimeException("Should not reach here")
+        }
+        i = nextI; j = nextJ
+    }
+
     return commands.length
 }
 
