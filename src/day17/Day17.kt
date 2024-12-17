66e52cfb37efc5d9027ef15ff8f1d759846477d9
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-17 14:57:33 +0400

    Day17, part2: Refactoring

--- src/day17/Day17.kt
+++ src/day17/Day17.kt
@@ -18,8 +18,6 @@ const val OUT = 5
 const val BDV = 6
 const val CDV = 7
 
-const val OUTPUT_LIMIT = 18
-
 fun perform(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): List<Int> {
     var aReg = aReg
     var bReg = bReg
@@ -90,33 +88,17 @@ fun perform(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): List<Int> {
 
             else -> throw RuntimeException("Should not reach here")
         }
-
-        if (output.size > OUTPUT_LIMIT) {
-            break
-        }
     }
 
     return output
-}
-
-fun check(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): Boolean {
-    return perform(aReg, bReg, cReg, program) == program
 }
 
 fun part1(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): String {
     return perform(aReg, bReg, cReg, program).joinToString(",")
 }
 
-fun part2(bReg: Int, cReg: Int, program: List<Int>): Int {
-    var a = 0
-    while (!check(a, bReg, cReg, program)) {
-        if (a % 1_000_000 == 0) {
-            println("A = $a")
-        }
-        a++
-        if (a < 0) break
-    }
-    return a
+fun part2(program: List<Int>): Int {
+    return -1
 }
 
 fun main() {
@@ -130,18 +112,9 @@ fun main() {
     }
 
     run {
-        val (registers, program) = readInput("Day17_test02").parse()
-        val (_, bReg, cReg) = registers
-
-        val expected = 117440
-        val actual = part2(bReg, cReg, program)
-        assertEquals(expected, actual)
-    }
-
-    run {
         val (registers, program) = readInput("Day17").parse()
         val (aReg, bReg, cReg) = registers
         part1(aReg, bReg, cReg, program).println()
-        part2(bReg, cReg, program).println()
+        part2(program).println()
     }
 }
