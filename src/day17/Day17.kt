82de6555912b6055ddc9f7afdc2b784c8f99107e
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-17 12:30:30 +0400

    Day17, part1: AC

--- src/day17/Day17.kt
+++ src/day17/Day17.kt
@@ -7,13 +7,81 @@ import readInput
 fun List<String>.parse() =
     take(3).map { it.split(": ").last().toInt() } to last().split(": ").last().split(",").map(String::toInt)
 
-fun part1(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): String {
-    println("A = $aReg")
-    println("B = $bReg")
-    println("C = $cReg")
-    println("program: $program")
+const val MOD = 8
 
-    return ""
+const val ADV = 0
+const val BXL = 1
+const val BST = 2
+const val JNZ = 3
+const val BXC = 4
+const val OUT = 5
+const val BDV = 6
+const val CDV = 7
+
+fun part1(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): String {
+    var aReg = aReg
+    var bReg = bReg
+    var cReg = cReg
+
+    val output = mutableListOf<Int>()
+
+    var pc = 0
+    while (pc < program.size) {
+        val opcode = program[pc]
+        val literalOperand = program[pc + 1]
+        val comboOperand = when (literalOperand) {
+            in 0..3 -> literalOperand
+            4 -> aReg
+            5 -> bReg
+            6 -> cReg
+            7 -> -1
+            else -> throw RuntimeException("Should not reach here")
+        }
+
+        fun dv() = aReg / (1 shl comboOperand)
+
+        when (opcode) {
+            ADV -> {
+                aReg = dv()
+                pc += 2
+            }
+            BXL -> {
+                bReg = bReg xor literalOperand
+                pc += 2
+            }
+            BST -> {
+                bReg = comboOperand % MOD
+                pc += 2
+            }
+            JNZ -> {
+                if (aReg != 0) {
+                    pc = literalOperand
+                } else {
+                    pc += 2
+                }
+            }
+            BXC -> {
+                bReg = bReg xor cReg
+                pc += 2
+            }
+            OUT -> {
+                output += comboOperand % MOD
+                pc += 2
+            }
+            BDV -> {
+                bReg = dv()
+                pc += 2
+            }
+            CDV -> {
+                cReg = dv()
+                pc += 2
+
+            }
+            else -> throw RuntimeException("Should not reach here")
+        }
+    }
+
+    return output.joinToString(",")
 }
 
 fun part2(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): String {
