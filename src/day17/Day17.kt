7832cb97a01f53b5f6f1b4a3c0f1096f4258f023
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-17 11:54:08 +0400

    Day17, part1: Parse data

--- src/day17/Day17.kt
+++ src/day17/Day17.kt
@@ -4,34 +4,44 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun List<String>.parse() =
+    take(3).map { it.split(": ").last().toInt() } to last().split(": ").last().split(",").map(String::toInt)
+
+fun part1(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): String {
+    println("A = $aReg")
+    println("B = $bReg")
+    println("C = $cReg")
+    println("program: $program")
+
+    return ""
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(aReg: Int, bReg: Int, cReg: Int, program: List<Int>): String {
+    return ""
 }
 
 fun main() {
     run {
-        val input = readInput("Day17_test01")
+        val (registers, program) = readInput("Day17_test01").parse()
+        val (aReg, bReg, cReg) = registers
 
         run {
-            val expected = -1
-            val actual = part1(input)
+            val expected = "4,6,3,5,6,3,5,2,1,0"
+            val actual = part1(aReg, bReg, cReg, program)
             assertEquals(expected, actual)
         }
 
         run {
-            val expected = -1
-            val actual = part2(input)
+            val expected = "^_^"
+            val actual = part2(aReg, bReg, cReg, program)
             assertEquals(expected, actual)
         }
     }
 
     run {
-        val input = readInput("Day17")
-        part1(input).println()
-        part2(input).println()
+        val (registers, program) = readInput("Day17").parse()
+        val (aReg, bReg, cReg) = registers
+        part1(aReg, bReg, cReg, program).println()
+        part2(aReg, bReg, cReg, program).println()
     }
 }
