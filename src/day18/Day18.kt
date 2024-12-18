e45163c68841bf1d3c16f516d400fa2cb4cd5534
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-18 10:38:12 +0400

    Day18: Parse data

--- src/day18/Day18.kt
+++ src/day18/Day18.kt
@@ -4,34 +4,43 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun List<String>.parse() = map { it.split(",").map(String::toInt).let { (x, y) -> x to y } }
+
+fun part1(n: Int, limit: Int, points: List<Pair<Int, Int>>): Int {
+    println(points)
+
+    return points.size
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(points: List<Pair<Int, Int>>): Int {
+    return points.size
 }
 
 fun main() {
     run {
-        val input = readInput("Day18_test01")
+        val n = 6
+        val limit = 12
+        val points = readInput("Day18_test01").parse()
 
         run {
-            val expected = -1
-            val actual = part1(input)
+            val expected = 22
+            val actual = part1(n, limit, points)
             assertEquals(expected, actual)
         }
 
         run {
             val expected = -1
-            val actual = part2(input)
+            val actual = part2(points)
             assertEquals(expected, actual)
         }
     }
 
     run {
-        val input = readInput("Day18")
-        part1(input).println()
-        part2(input).println()
+        val n = 70
+        val limit = 1024
+        val points = readInput("Day18").parse()
+
+        part1(n, limit, points).println()
+        part2(points).println()
     }
 }
