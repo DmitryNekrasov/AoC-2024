b2cd290decd8a6b4d52b55eaeb3616891d643556
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-18 10:49:53 +0400

    Day18, part1: AC

--- src/day18/Day18.kt
+++ src/day18/Day18.kt
@@ -3,13 +3,36 @@ package day18
 import assertEquals
 import println
 import readInput
+import java.util.LinkedList
+import java.util.Queue
 
 fun List<String>.parse() = map { it.split(",").map(String::toInt).let { (x, y) -> x to y } }
 
 fun part1(n: Int, limit: Int, points: List<Pair<Int, Int>>): Int {
-    println(points)
+    val grid = Array(n) { CharArray(n) { '.' } }
+    for (i in 0..<limit) {
+        val (x, y) = points[i]
+        grid[y][x] = '#'
+    }
 
-    return points.size
+    val visited = Array(n) { BooleanArray(n) }
+    val queue: Queue<List<Int>> = LinkedList()
+    queue.offer(listOf(0, 0, 0))
+    visited[0][0] = true
+    while (queue.isNotEmpty()) {
+        val (x, y, distance) = queue.poll()
+        if (x == n - 1 && y == n - 1) return distance
+        for ((dx, dy) in listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)) {
+            val nextX = x + dx
+            val nextY = y + dy
+            if (nextX in 0..<n && nextY in 0..<n && grid[nextY][nextX] == '.' && !visited[nextY][nextX]) {
+                visited[nextY][nextX] = true
+                queue.offer(listOf(nextX, nextY, distance + 1))
+            }
+        }
+    }
+
+    throw RuntimeException("Should not reach here")
 }
 
 fun part2(points: List<Pair<Int, Int>>): Int {
@@ -18,7 +41,7 @@ fun part2(points: List<Pair<Int, Int>>): Int {
 
 fun main() {
     run {
-        val n = 6
+        val n = 7
         val limit = 12
         val points = readInput("Day18_test01").parse()
 
@@ -36,7 +59,7 @@ fun main() {
     }
 
     run {
-        val n = 70
+        val n = 71
         val limit = 1024
         val points = readInput("Day18").parse()
 
