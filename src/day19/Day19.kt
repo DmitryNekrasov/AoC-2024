8a2f0d85230593d7c045a850803b377b0b719599
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-19 10:13:08 +0400

    Day19: Parse data

--- src/day19/Day19.kt
+++ src/day19/Day19.kt
@@ -4,34 +4,39 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun List<String>.parse() = first().split(",") to slice(2..lastIndex)
+
+fun part1(patterns: List<String>, designs: List<String>): Int {
+    println(patterns)
+    println(designs.joinToString("\n"))
+
+    return patterns.size
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(patterns: List<String>, designs: List<String>): Int {
+    return patterns.size
 }
 
 fun main() {
     run {
-        val input = readInput("Day19_test01")
+        val (patterns, designs) = readInput("Day19_test01").parse()
 
         run {
-            val expected = -1
-            val actual = part1(input)
+            val expected = 6
+            val actual = part1(patterns, designs)
             assertEquals(expected, actual)
         }
 
         run {
             val expected = -1
-            val actual = part2(input)
+            val actual = part2(patterns, designs)
             assertEquals(expected, actual)
         }
     }
 
     run {
-        val input = readInput("Day19")
-        part1(input).println()
-        part2(input).println()
+        val (patterns, designs) = readInput("Day19").parse()
+        part1(patterns, designs).println()
+        part2(patterns, designs).println()
     }
 }
