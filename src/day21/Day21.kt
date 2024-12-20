fbae451f954c31e64c582b2d2ed043a0d082e9c3
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-20 13:24:21 +0400

    Day21: Initial

---
+++ src/day21/Day21.kt
@@ -0,0 +1,37 @@
+package day21
+
+import assertEquals
+import println
+import readInput
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
+
+fun main() {
+    run {
+        val input = readInput("Day21_test01")
+
+        run {
+            val expected = -1
+            val actual = part1(input)
+            assertEquals(expected, actual)
+        }
+
+        run {
+            val expected = -1
+            val actual = part2(input)
+            assertEquals(expected, actual)
+        }
+    }
+
+    run {
+        val input = readInput("Day21")
+        part1(input).println()
+        part2(input).println()
+    }
+}
