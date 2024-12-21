89b32ec9497936780d924c365b00ad612cc13ede
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-21 14:49:11 +0400

    Day21, part1: Add buildVertex method

--- src/day21/Day21.kt
+++ src/day21/Day21.kt
@@ -4,7 +4,40 @@ import assertEquals
 import println
 import readInput
 
+data class Vertex(val up: Long, val down: Long, val left: Long, val right: Long)
+
+fun HashMap<Long, MutableList<Long>>.add(from: Long, to: Long) {
+    getOrPut(from) { mutableListOf() } += to
+}
+
 fun part1(input: List<String>): Int {
+    val graph = HashMap<Long, MutableList<Long>>()
+
+    fun buildVertex(depth: Int, id: Long): Vertex {
+        if (depth == 0) {
+            return Vertex(id, id, id, id)
+        }
+
+        val a = buildVertex(depth - 1, id * 5)
+        val up = buildVertex(depth - 1, id * 5 + 1)
+        val down = buildVertex(depth - 1, id * 5 + 2)
+        val left = buildVertex(depth - 1, id * 5 + 3)
+        val right = buildVertex(depth - 1, id * 5 + 4)
+
+        graph.add(left.right, down.right)
+        graph.add(down.left, left.left)
+        graph.add(down.up, up.up)
+        graph.add(up.down, down.down)
+        graph.add(down.right, right.right)
+        graph.add(right.left, down.left)
+        graph.add(up.right, a.right)
+        graph.add(a.left, up.left)
+        graph.add(right.up, a.up)
+        graph.add(a.down, right.down)
+
+        return Vertex(up.up, down.down, left.left, right.right)
+    }
+
     return input.size
 }
 
