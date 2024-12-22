755cbc866fde9a85dea1464febc7f7f741681e01
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-22 14:27:16 +0400

    Day22, part1: AC

--- src/day22/Day22.kt
+++ src/day22/Day22.kt
@@ -4,20 +4,33 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<Int>): Int {
-    return input.size
+const val MOD = 16777216L
+
+fun Long.mixInto(other: Long) = this xor other
+
+fun Long.prune() = this % MOD
+
+fun Long.nextSecretNumber(): Long {
+    val step1 = (this * 64).mixInto(this).prune()
+    val step2 = (step1 / 32).mixInto(step1).prune()
+    val step3 = (step2 * 2048).mixInto(step2).prune()
+    return step3
 }
 
-fun part2(input: List<Int>): Int {
+fun part1(nums: List<Long>): Long {
+    return nums.sumOf { num -> (0..<2000).fold(num) { acc, _ -> acc.nextSecretNumber() } }
+}
+
+fun part2(input: List<Long>): Int {
     return input.size
 }
 
 fun main() {
     run {
-        val input = readInput("Day22_test01").map(String::toInt)
+        val input = readInput("Day22_test01").map(String::toLong)
 
         run {
-            val expected = -1
+            val expected = 37327623L
             val actual = part1(input)
             assertEquals(expected, actual)
         }
@@ -30,7 +43,7 @@ fun main() {
     }
 
     run {
-        val input = readInput("Day22").map(String::toInt)
+        val input = readInput("Day22").map(String::toLong)
         part1(input).println()
         part2(input).println()
     }
