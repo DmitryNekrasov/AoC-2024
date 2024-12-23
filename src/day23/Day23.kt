5543db487bd40f366045915860d6312455c311ed
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-23 10:20:52 +0400

    Day23, part1: AC

--- src/day23/Day23.kt
+++ src/day23/Day23.kt
@@ -6,10 +6,31 @@ import readInput
 
 fun List<String>.parse(): List<Pair<String, String>> = map { it.split("-").let { it.first() to it.last() } }
 
-fun part1(edges: List<Pair<String, String>>): Int {
-    println(edges.joinToString("\n"))
+fun HashMap<String, MutableSet<String>>.add(from: String, to: String) {
+    getOrPut(from) { mutableSetOf() } += to
+}
 
-    return edges.size
+fun part1(edges: List<Pair<String, String>>): Int {
+    val graph = HashMap<String, MutableSet<String>>()
+    for ((from, to) in edges) {
+        graph.add(from, to)
+        graph.add(to, from)
+    }
+
+    val triangles = HashSet<List<String>>()
+    for ((from, neighbours) in graph) {
+        if (from.startsWith('t')) {
+            for (u in neighbours) {
+                for (v in neighbours) {
+                    if (graph[v]?.contains(u) == true) {
+                        triangles += listOf(from, u, v).sorted()
+                    }
+                }
+            }
+        }
+    }
+
+    return triangles.size
 }
 
 fun part2(edges: List<Pair<String, String>>): Int {
@@ -21,7 +42,7 @@ fun main() {
         val edges = readInput("Day23_test01").parse()
 
         run {
-            val expected = -1
+            val expected = 7
             val actual = part1(edges)
             assertEquals(expected, actual)
         }
