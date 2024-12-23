03b490fd4fb9d509e4b7dd475f7585d630653483
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-23 10:07:15 +0400

    Day23: Parse data

--- src/day23/Day23.kt
+++ src/day23/Day23.kt
@@ -4,34 +4,38 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun List<String>.parse(): List<Pair<String, String>> = map { it.split("-").let { it.first() to it.last() } }
+
+fun part1(edges: List<Pair<String, String>>): Int {
+    println(edges.joinToString("\n"))
+
+    return edges.size
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(edges: List<Pair<String, String>>): Int {
+    return edges.size
 }
 
 fun main() {
     run {
-        val input = readInput("Day23_test01")
+        val edges = readInput("Day23_test01").parse()
 
         run {
             val expected = -1
-            val actual = part1(input)
+            val actual = part1(edges)
             assertEquals(expected, actual)
         }
 
         run {
             val expected = -1
-            val actual = part2(input)
+            val actual = part2(edges)
             assertEquals(expected, actual)
         }
     }
 
     run {
-        val input = readInput("Day23")
-        part1(input).println()
-        part2(input).println()
+        val edges = readInput("Day23").parse()
+        part1(edges).println()
+        part2(edges).println()
     }
 }
