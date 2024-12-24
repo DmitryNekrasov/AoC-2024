2afc29536066dd77cc8159dc40924fc5af3b0a78
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-24 10:47:52 +0400

    Day24, part1: Add Node, Unary, Binary

--- src/day24/Day24.kt
+++ src/day24/Day24.kt
@@ -3,21 +3,39 @@ package day24
 import assertEquals
 import println
 import readInput
+import shouldNotReachHere
 
-fun List<String>.parse(): Pair<List<Pair<String, Int>>, List<Pair<String, List<String>>>> {
+fun List<String>.parse(): Pair<Map<String, Int>, Map<String, List<String>>> {
     val pivot = indexOf("")
-    return slice(0..<pivot).map { it.split(": ").let { (s, v) -> s to v.toInt() } } to
-            slice((pivot + 1)..lastIndex).map { it.split(" -> ").let { (to, from) -> from to to.split(" ") } }
+    return slice(0..<pivot).associate { it.split(": ").let { (s, v) -> s to v.toInt() } } to
+            slice((pivot + 1)..lastIndex).associate { it.split(" -> ").let { (to, from) -> from to to.split(" ") } }
 }
 
-fun part1(unary: List<Pair<String, Int>>, binary: List<Pair<String, List<String>>>): Int {
-    unary.joinToString("\n").println()
-    binary.joinToString("\n").println()
+interface Node {
+    fun perform(): Int
+}
+
+class Unary(val value: Int) : Node {
+    override fun perform() = value
+}
+
+class Binary(val lhs: Node, val rhs: Node, val operation: String) : Node {
+    override fun perform() = when (operation) {
+        "AND" -> lhs.perform() and rhs.perform()
+        "OR" -> lhs.perform() or rhs.perform()
+        "XOR" -> lhs.perform() xor rhs.perform()
+        else -> shouldNotReachHere()
+    }
+}
+
+fun part1(unary: Map<String, Int>, binary: Map<String, List<String>>): Int {
+    unary.entries.joinToString("\n").println()
+    binary.entries.joinToString("\n").println()
 
     return unary.size
 }
 
-fun part2(unary: List<Pair<String, Int>>, binary: List<Pair<String, List<String>>>): Int {
+fun part2(unary: Map<String, Int>, binary: Map<String, List<String>>): Int {
     return unary.size
 }
 
