26e69298767591c729558ad407daa7dce80f9ff8
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-24 10:36:18 +0400

    Day24, part1: Parse data

--- src/day24/Day24.kt
+++ src/day24/Day24.kt
@@ -4,34 +4,43 @@ import assertEquals
 import println
 import readInput
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun List<String>.parse(): Pair<List<Pair<String, Int>>, List<Pair<String, List<String>>>> {
+    val pivot = indexOf("")
+    return slice(0..<pivot).map { it.split(": ").let { (s, v) -> s to v.toInt() } } to
+            slice((pivot + 1)..lastIndex).map { it.split(" -> ").let { (to, from) -> from to to.split(" ") } }
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part1(unary: List<Pair<String, Int>>, binary: List<Pair<String, List<String>>>): Int {
+    unary.joinToString("\n").println()
+    binary.joinToString("\n").println()
+
+    return unary.size
+}
+
+fun part2(unary: List<Pair<String, Int>>, binary: List<Pair<String, List<String>>>): Int {
+    return unary.size
 }
 
 fun main() {
     run {
-        val input = readInput("Day24_test01")
+        val (unary, binary) = readInput("Day24_test01").parse()
 
         run {
             val expected = 2024
-            val actual = part1(input)
+            val actual = part1(unary, binary)
             assertEquals(expected, actual)
         }
 
         run {
             val expected = -1
-            val actual = part2(input)
+            val actual = part2(unary, binary)
             assertEquals(expected, actual)
         }
     }
 
     run {
-        val input = readInput("Day24")
-        part1(input).println()
-        part2(input).println()
+        val (unary, binary) = readInput("Day24").parse()
+        part1(unary, binary).println()
+        part2(unary, binary).println()
     }
 }
