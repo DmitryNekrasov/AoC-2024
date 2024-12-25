57cca73569475158c78f024136dbabab7764bc41
Dmitry Nekrasov <scannorone@gmail.com>
2024-12-25 10:35:43 +0400

    Day25: Initial

---
+++ src/day25/Day25.kt
@@ -0,0 +1,37 @@
+package day25
+
+import assertEquals
+import println
+import readInput
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
+
+fun main() {
+    run {
+        val input = readInput("Day25_test01")
+
+        run {
+            val expected = -1
+            val actual = part1(input)
+            assertEquals(expected, actual)
+        }
+
+        run {
+            val expected = -1
+            val actual = part2(input)
+            assertEquals(expected, actual)
+        }
+    }
+
+    run {
+        val input = readInput("Day25")
+        part1(input).println()
+        part2(input).println()
+    }
+}
